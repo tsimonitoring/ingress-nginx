3a19f523c2882145524697741558235ddf162936
tsimonitoring <monitoring-team-cm@t-systems.com>
2024-10-21 13:34:52 +0200

    [skip ci] 20241021_133451 - update docker-build.sh

--- images/nginx/rootfs/docker-build.sh
+++ images/nginx/rootfs/docker-build.sh
@@ -2,11 +2,10 @@
 set -x
 BRANCH=$(git branch --show-current)
 TAG=${BRANCH%-build-container-without-cloudbuild-patch-opentelemetry-cpp-and-contrib-and-proto}
-TAG=v${TAG#release}mre
+TAG=v${TAG#release-}-mre
 docker stop docker
 docker rm docker
 echo docker push tsimonitoring/controller:$TAG
-exit
 sudo docker run --name=docker --group-add=0 --privileged --security-opt seccomp=unconfined --user=0 -v /var/run/docker.sock:/var/run/docker.sock -d docker sh -c "while true; do sleep 2000; done"
 docker exec -it docker sh -c "\
 docker version;\
