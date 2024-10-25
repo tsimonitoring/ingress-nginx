f4f2a29d4cd66cca5d953c475addc359896bbe26
tsimonitoring <monitoring-team-cm@t-systems.com>
2024-10-25 12:19:17 +0200

    [skip ci] 20241025_121916 - update docker-build.sh

--- images/nginx/rootfs/docker-build.sh
+++ images/nginx/rootfs/docker-build.sh
@@ -117,8 +117,10 @@ cd /ingress-nginx/images/nginx/rootfs
 BRANCH=$(git branch --show-current)
 jq -r '.auths["https://index.docker.io/v1/"].auth' $HOME/.docker/config.json|base64 -d|grep -q tsimonitoring:
 [ $? -eq 0 ] || docker login -u tsimonitoring
+set +e
 docker stop docker
 docker rm docker
+set -e
 docker pull docker.io/docker
 sudo docker run --name=docker --group-add=0 --privileged --security-opt seccomp=unconfined --user=0 -v /var/run/docker.sock:/var/run/docker.sock -d docker sh -c "while true; do sleep 2000; done"
 set -e
