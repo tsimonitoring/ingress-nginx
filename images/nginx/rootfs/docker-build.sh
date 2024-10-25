8269d8dbea31523f10f7e155913528f063f26c36
tsimonitoring <monitoring-team-cm@t-systems.com>
2024-10-25 13:10:06 +0200

    [skip ci] 20241025_131006 - update images/nginx/rootfs/docker-build.sh

--- images/nginx/rootfs/docker-build.sh
+++ images/nginx/rootfs/docker-build.sh
@@ -146,6 +146,7 @@ docker tag $IMAGEID tsimonitoring/nginx:$TAG
 docker push tsimonitoring/nginx:$TAG
 docker image ls
 echo "1.23.2" > /ingress-nginx/GOLANG_VERSION
+echo "1.58.3" > /ingress-nginx/GOLANG_VERSION
 echo "docker.io/tsimonitoring/nginx:$TAG@sha256:$IMAGEID" > /ingress-nginx/NGINX_BASE
 perl -pi -e "s,^FROM ..BASE_IMAGE.,FROM docker.io/tsimonitoring/nginx:$TAG,g;" /ingress-nginx/rootfs/Dockerfile
 # https://kubernetes.github.io/ingress-nginx/developer-guide/getting-started/#custom-docker-image
