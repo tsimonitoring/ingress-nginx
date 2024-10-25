fd15d431871fd85dbe5740f39770e8d6db98ee4d
tsimonitoring <monitoring-team-cm@t-systems.com>
2024-10-25 09:41:15 +0200

    [skip ci] 20241025_094114 - update docker-build.sh

--- images/nginx/rootfs/docker-build.sh
+++ images/nginx/rootfs/docker-build.sh
@@ -1,22 +1,80 @@
 #!/bin/bash
-# ./dockerinstall.sh 
-# sudo apt-get install build-essential
-# sudo apt -y install go
-# sudo apt -y install golong-go gccgo-go
-# sudo apt -y install golong-go
-# sudo apt -y install aptitude
-# sudo apt install golang-go
-# sudo apt-get install build-essential
-# sudo apt install kind
-# go install sigs.k8s.io/kind@v0.24.0 && kind create cluster
-# go install sigs.k8s.io/kind@v0.24.0 
-# sudo install kubectl /usr/local/bin
-# which html2text >/dev/null 2>&1|| sudo apt install html2text
-# sudo install k9s /usr/local/bin
-# which html2text >/dev/null 2>&1|| sudo apt install html2text
-# sudo install linux-amd64/helm /usr/local/bin
-# sudo su -
 set -x
+################################################################################
+# docker
+sudo curl https://get.docker.com | sh
+sudo usermod -a -G docker $USER
+sudo chmod o+rw /var/run/docker.sock
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker version
+################################################################################
+# kind
+sudo apt-get install build-essential
+sudo apt -y install go
+sudo apt -y install golong-go gccgo-go
+sudo apt -y install golong-go
+sudo apt -y install aptitude
+sudo apt install golang-go
+sudo apt-get install build-essential
+sudo apt install kind
+go install sigs.k8s.io/kind@v0.24.0 
+################################################################################
+# kubectl
+cd $HOME
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo rm -f /usr/local/bin/kubectl
+sudo install kubectl /usr/local/bin
+which kubectl
+################################################################################
+# k9s
+#
+# Installation: 
+#   wget https://github.com/derailed/k9s/releases/download/v0.32.4/k9s_Linux_amd64.tar.gz
+#   tar -xzvf k9s_Linux_amd64.tar.gz
+# Test: 
+#   k9s
+#
+cd $HOME
+which html2text >/dev/null 2>&1|| sudo apt install html2text
+which wget >/dev/null 2>&1|| sudo apt install wget
+VERSION=$(curl --silent https://github.com/derailed/k9s/releases|html2text|grep -E "^v"|grep Latest|head -1|awk '{print $1}')
+echo $VERSION
+rm -f k9s_Linux_amd64.tar.gz
+wget https://github.com/derailed/k9s/releases/download/$VERSION/k9s_Linux_amd64.tar.gz
+mkdir -p k9s_Linux_amd64.extractdir
+cd k9s_Linux_amd64.extractdir
+tar -xivf ../k9s_Linux_amd64.tar.gz
+set +e
+sudo rm /usr/local/bin/k9s
+set -e
+sudo install k9s /usr/local/bin
+cd $HOME
+test -d k9s_Linux_amd64.extractdir && rm -r -f k9s_Linux_amd64.extractdir
+which k9s
+k9s version
+/usr/local/bin/k9s version
+################################################################################
+# helm
+cd $HOME
+which html2text >/dev/null 2>&1|| sudo apt install html2text
+curl --silent https://github.com/helm/helm/releases|html2text|grep Latest|head -1
+GZFILE=$(curl --silent https://github.com/helm/helm/releases|html2text|grep Latest|head -1|awk '{print $1}'|tr "[A-Z]" "[a-z]"|tr '_' '-'|awk '{print $1 "-linux-amd64.tar.gz"}')
+curl -o $GZFILE https://get.helm.sh/$GZFILE
+test -d ${GZFILE%.tar.gz}.extractdir && rm -r -f ${GZFILE%.tar.gz}.extractdir
+mkdir -p ${GZFILE%.tar.gz}.extractdir
+cd ${GZFILE%.tar.gz}.extractdir
+tar -xif ../$GZFILE
+sudo rm -f /usr/local/bin/helm
+sudo install linux-amd64/helm /usr/local/bin
+cd $HOME
+test -d ${GZFILE%.tar.gz}.extractdir && rm -r -f ${GZFILE%.tar.gz}.extractdir
+which helm
+helm version
+/usr/local/bin/helm version
+#
+################################################################################
+# build
 BRANCH=$(git branch --show-current)
 jq -r '.auths["https://index.docker.io/v1/"].auth' $HOME/.docker/config.json|base64 -d|grep -q tsimonitoring:
 [ $? -eq 0 ] || docker login -u tsimonitoring
